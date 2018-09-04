6fd0a2fa62aa3db53f10a97fea55874736b9d2b1
Alexandr Egorov <avegrv@gmail.com>
2018-09-04 22:46:38 +0400

    [Common] Init repo

---
+++ sample/src/main/java/com/drake/sample/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.drake.sample
+
+import android.os.Bundle
+import android.os.Handler
+import android.support.v7.app.AppCompatActivity
+import com.drake.R
+import com.drake.lib.sd.LoadingStateDelegate
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
